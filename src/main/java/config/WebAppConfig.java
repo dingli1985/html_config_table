6845d0774a03effc7db9c5dd1bec437ae7ba57ea
alee <qq37589412@163.com>
2020-01-24 09:56:04 +0800

    修改路径

--- src/main/java/config/WebAppConfig.java
+++ src/main/java/config/WebAppConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @Configuration
-@ComponentScan("com.xhp.hrms.web")
+@ComponentScan("web")
 @EnableWebMvc
 @MapperScan("com.xhp.hrms.mapper")
 @PropertySource("classpath:application.properties") // 读取application.properties
