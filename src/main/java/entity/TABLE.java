ceb01854711d8301d109cc1c838c9ab0d2b13df0
alee <qq37589412@163.com>
2020-01-24 09:56:47 +0800

    修改路径

---
+++ src/main/java/entity/TABLE.java
@@ -0,0 +1,39 @@
+package entity;
+
+import java.util.List;
+
+public class TABLE {
+
+	String tableCode;
+
+	String tableName;
+
+	List<TR> list;
+
+	public String getTableCode() {
+		return tableCode;
+	}
+
+	public void setTableCode(String tableCode) {
+		this.tableCode = tableCode;
+	}
+
+	public String getTableName() {
+		return tableName;
+	}
+
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+
+	public List<TR> getList() {
+		return list;
+	}
+
+	public void setList(List<TR> list) {
+		this.list = list;
+	}
+
+	
+
+}
